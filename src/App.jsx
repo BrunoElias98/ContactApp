d43cf83525c8f28777ecfecc2a8c28e2a3c6dab8
brunelias98 <bruno.elias3312@gmail.com>
2020-05-16 16:45:54 -0300

    Criado as funções de ordenação e passando por props essas funções como callback.

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,6 @@ function App() {
   const [contacts, setContacts] = useState([]);
   const [showContacts, setShowContacts] = useState([]);
   
-  const filterList = event => {
-    let updatedList = contacts.filter(function(item){
-      return item.name.toLowerCase().includes(event.target.value.toLowerCase());
-    });
-    
-    setShowContacts(updatedList)
-  }
-
   useEffect(() => {
     fetch('https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts')
     .then((response) => response.json())
@@ -25,11 +17,159 @@ function App() {
     });
   }, []);
 
+  const filterList = event => {
+    let updatedList = contacts.filter(function(item){
+      return item.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    
+    setShowContacts(updatedList)
+  }
+
+  const sortByName = crescent => {
+    if(crescent) {
+      setShowContacts(contacts.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+  
+        if (a.name > b.name) {
+          return 1;
+        }
+  
+        return 0;
+      }));
+    } else {
+      setShowContacts([...contacts].sort((a, b) => {
+        if (a.name > b.name) {
+          return -1;
+        }
+  
+        if (a.name < b.name) {
+          return 1;
+        }
+  
+        return 0;
+      }))
+    }
+  }
+
+  const sortByCountry = crescent => {
+    if(crescent) {
+      setShowContacts(contacts.sort((a, b) => {
+        if (a.country < b.country) {
+          return -1;
+        }
+
+        if (a.country > b.country) {
+          return 1;
+        }
+
+        return 0;
+      }));
+    } else {
+      setShowContacts([...contacts].sort((a,b) => {
+        if (a.country > b.country) {
+          return -1;
+        }
+
+        if (a.country < b.country) {
+          return 1;
+        }
+
+        return 0;
+      }));
+    }
+  }
+
+  const sortByCompany = crescent => {
+    if (crescent) {
+      setShowContacts(contacts.sort((a, b) => {
+        if (a.company < b.company) {
+          return -1;
+        }
+
+        if (a.company > b.company) {
+          return 1;
+        }
+
+        return 0; 
+      }));
+    } else {
+      setShowContacts([...contacts].sort((a,b) => {
+        if (a.company > b.company) {
+          return -1;
+        }
+
+        if (a.company < b.company) {
+          return 1;
+        }
+
+        return 0; 
+      }));
+    }
+  }
+
+  const sortByDepartment = crescent => {
+    if (crescent) {
+      setShowContacts(contacts.sort((a, b) => {
+        if (a.department < b.department) {
+          return -1;
+        }
+
+        if (a.department > b.department) {
+          return 1;
+        }
+
+        return 0; 
+      }));
+    } else {
+      setShowContacts([...contacts].sort((a,b) => {
+        if (a.department > b.department) {
+          return -1;
+        }
+
+        if (a.department < b.department) {
+          return 1;
+        }
+
+        return 0; 
+      }));
+    }
+  }
+
+  const sortByAdmissionDate = crescent => {
+    if (crescent) {
+      setShowContacts(contacts.sort((a, b) => {
+        if (a.admissionDate < b.admissionDate) {
+          return -1;
+        }
+
+        if (a.admissionDate > b.admissionDate) {
+          return 1;
+        }
+
+        return 0; 
+      }));
+    } else {
+      setShowContacts([...contacts].sort((a,b) => {
+        if (a.admissionDate > b.admissionDate) {
+          return -1;
+        }
+
+        if (a.admissionDate < b.admissionDate) {
+          return 1;
+        }
+
+        return 0; 
+      }));
+    }
+  }
+
   return (
     <React.Fragment>
       <Topbar />
-      <Filters onChange={filterList} />
-      <Contacts data={showContacts}/>
+      <Filters functionCallback={{ filterList, sortByName, sortByCountry, sortByCompany, sortByDepartment, sortByAdmissionDate }} />
+      <Contacts contactData={showContacts}/>
     </React.Fragment>
   )
 }
